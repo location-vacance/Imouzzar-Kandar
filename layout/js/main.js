129e6d14fd0545f4a614a3df429c7bc0adaffd69
Marouane Bouchettoy <uanemaro216@gmail.com>
2023-07-08 20:14:11 +0100

    update

--- layout/js/main.js
+++ layout/js/main.js
@@ -16,12 +16,23 @@ var div = document.createElement("div");
 div.id = "btnWhatsappPopUp";
 var anchor = document.createElement("a");
 anchor.href = "https://api.whatsapp.com/send?phone=212663661627";
+anchor.title = "Whatsapp";
 var image = document.createElement("img");
 image.src = "./layout/image/whatsapp.png";
 anchor.appendChild(image);
 div.appendChild(anchor);
 document.body.appendChild(div);
 
+var div2 = document.createElement("div");
+div2.id = "btnTelephonePopUp";
+var anchor2 = document.createElement("a");
+anchor2.href = "tel:+212663661627";
+anchor2.title = "Telephone";
+var image2 = document.createElement("img");
+image2.src = "./layout/image/telephone.png";
+anchor2.appendChild(image2);
+div2.appendChild(anchor2);
+document.body.appendChild(div2);
 
 function showImage(id,folder){
     var path = `./layout/image/${folder}/i-${id}.jpg`
